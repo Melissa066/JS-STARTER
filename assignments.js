c8e0330a4052dab60ab27d7926d246131fa8162f
Melissa066 <Melissa066@users.noreply.github.com>
2024-02-02 19:05:14 +0000

    :+1:

---
+++ assignments.js
@@ -0,0 +1,8 @@
+let country = "Romania"
+console.log("Romania")
+
+let continent = "EU"
+console.log("EU")
+    
+let population = "8"
+console.log(8)
---
+++ script.js
@@ -0,0 +1,10 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Melissa");
+console.log("24");
+
+let fisrtName = "Melissa";
+console.log("firstName");
+
+let myFisrtJob = "Programmer";
