468fb3f24b96dc7cf21ac922ce9784d221a8ab32
Melissa066 <Melissa066@users.noreply.github.com>
2024-02-21 02:55:30 +0000

    :+1:

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Melissa");
-console.log("24");
+// console.log("Melissa");
+// console.log("24");
 
-let fisrtName = "Melissa";
-console.log("firstName");
+// let fisrtName = "Melissa";
+// console.log("firstName");
 
-let myFisrtJob = "Programmer";
+// let myFisrtJob = "Programmer";
+
+
+
+
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Melisa');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+
+let age = 30;
+age = 31;
+
+
+const birth = 1991;
+// birth = 1990;
+
+const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+
+lastName = 'Melisa';
+console.log(lastName);
